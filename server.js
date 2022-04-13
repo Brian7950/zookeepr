688b336a5042c7a320a49eab9d57fd8b5c69f519
Brian7950 <brianh7950@gmail.com>
2022-04-12 22:32:11 -0600

    add Heroku

--- server.js
+++ server.js
@@ -2,14 +2,60 @@ const { animals } = require('./data/animals.json');
 const express = require('express');
 const app = express();
 
+//created function to take req.query argument in app.get function
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    // Note that we save the animalsArray as filteredResults here:
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+      // Save personalityTraits as a dedicated array.
+      // If personalityTraits is a string, place it into a new array and save.
+      if (typeof query.personalityTraits === 'string') {
+        personalityTraitsArray = [query.personalityTraits];
+      } else {
+        personalityTraitsArray = query.personalityTraits;
+      }
+      // Loop through each trait in the personalityTraits array:
+      personalityTraitsArray.forEach(trait => {
+        // Check the trait against each animal in the filteredResults array.
+        // Remember, it is initially a copy of the animalsArray,
+        // but here we're updating it for each trait in the .forEach() loop.
+        // For each trait being targeted by the filter, the filteredResults
+        // array will then contain only the entries that contain the trait,
+        // so at the end we'll have an array of animals that have every one 
+        // of the traits when the .forEach() loop is finished.
+        filteredResults = filteredResults.filter(
+          animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+      });
+    }
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    // return the filtered results:
+    return filteredResults;
+  }
+
 //get method req. 2 arguments  1st str describes route client will have to fetch from
 //second callback function taht will execute everytime route is accessed with a GET request
-app.get('/animals', (req,res) =>{
+app.get('/api/animals', (req, res) => {
+    //first parameter is request
     //using send method from res Parameter to send strings like hello! to client / res Obj has a lot more methods then send
     //to send JSON change send() to json()
-    res.json('animals')
+    let results = animals;
+    if(req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results)
 });
 
-app.listen(3001, () =>{
+app.listen(3001, () => {
     console.log(`API Server now on port 3001!`)
 });
+
