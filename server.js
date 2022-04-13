f8cbf1ee196c3ed114c6338d2829e344ec48da85
Brian7950 <brianh7950@gmail.com>
2022-04-12 22:43:37 -0600

    heroku set up

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals.json');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //created function to take req.query argument in app.get function
@@ -55,7 +56,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results)
 });
 
-app.listen(3001, () => {
-    console.log(`API Server now on port 3001!`)
+app.listen(PORT, () => {
+    console.log(`API Server now on port ${PORT}!`)
 });
 
