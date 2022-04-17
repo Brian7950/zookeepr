15eec2e254fd1eb82b708c8c0dbc54d155e257bb
Brian7950 <brianh7950@gmail.com>
2022-04-16 23:29:18 -0600

    added zookeeper to app

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
 
@@ -16,134 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
 
-//created function to take req.query argument in app.get function
-function filterByQuery(query, animalsArray) {
-  let personalityTraitsArray = [];
-  // Note that we save the animalsArray as filteredResults here:
-  let filteredResults = animalsArray;
-  if (query.personalityTraits) {
-    // Save personalityTraits as a dedicated array.
-    // If personalityTraits is a string, place it into a new array and save.
-    if (typeof query.personalityTraits === 'string') {
-      personalityTraitsArray = [query.personalityTraits];
-    } else {
-      personalityTraitsArray = query.personalityTraits;
-    }
-    // Loop through each trait in the personalityTraits array:
-    personalityTraitsArray.forEach(trait => {
-      // Check the trait against each animal in the filteredResults array.
-      // Remember, it is initially a copy of the animalsArray,
-      // but here we're updating it for each trait in the .forEach() loop.
-      // For each trait being targeted by the filter, the filteredResults
-      // array will then contain only the entries that contain the trait,
-      // so at the end we'll have an array of animals that have every one 
-      // of the traits when the .forEach() loop is finished.
-      filteredResults = filteredResults.filter(
-        animal => animal.personalityTraits.indexOf(trait) !== -1
-      );
-    });
-  }
-  if (query.diet) {
-    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-  }
-  if (query.species) {
-    filteredResults = filteredResults.filter(animal => animal.species === query.species);
-  }
-  if (query.name) {
-    filteredResults = filteredResults.filter(animal => animal.name === query.name);
-  }
-  // return the filtered results:
-  return filteredResults;
-}
+app.use('/api', apiRoutes);
 
-function findById(id, animalsArray) {
-  const result = animalsArray.filter(animal => animal.id === id)[0];
-  return result;
-}
-
-function createNewAnimal(body, animalsArray) {
-  const animal = body;
-  animalsArray.push(animal)
-  fs.writeFileSync(
-    path.join(__dirname, './data/animals.json'),
-    JSON.stringify({ animals: animalsArray }, null, 2)
-  );
-
-  return animal;
-}
-
-function validateAnimal(animal) {
-  if (!animal.name || typeof animal.name !== 'string') {
-    return false;
-  }
-
-  if (!animal.species || typeof animal.species !== 'string') {
-    return false
-  }
-
-  if (!animal.diet || typeof animal.diet !== 'string') {
-    return false
-  }
-
-  if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
-    return false
-  }
-
-  return true
-
-}
-
-
-
-//get method req. 2 arguments  1st str describes route client will have to fetch from
-//second callback function that will execute everytime route is accessed with a GET request
-app.get('/api/animals', (req, res) => {
-  //first parameter is request
-  //using send method from res Parameter to send strings like hello! to client / res Obj has a lot more methods then send
-  //to send JSON change send() to json()
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results)
-});
-
-app.get('/api/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-app.post('/api/animals', (req, res) => {
-  //set id based on what the next index of the arry will be
-  req.body.id = animals.length.toString();
-
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
-  } else {
-
-    // add animla to json file and animals array in this function 
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-  }
-});
-
-app.get('/',(req, res) =>{
-  res.sendFile(path.join(__dirname,'./public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API Server now on port ${PORT}!`)
